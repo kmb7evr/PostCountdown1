f769e61927e5612f8ed7d2a0409652e7ef99ed23
kmb7evr <kmb7evr@virginia.edu>
2022-03-29 23:09:53 -0400

    add feature 1

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 function Square(props) {
     return (
       <button className="square" onClick={props.onClick}>
@@ -55,6 +56,17 @@ function Square(props) {
     }
   
     handleClick(i) {
+        const locations = [
+            [1, 1],
+            [2, 1],
+            [3, 1],
+            [1, 2],
+            [2, 2],
+            [3, 2],
+            [1, 3],
+            [2, 3],
+            [3, 3]
+          ];
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
@@ -64,7 +76,8 @@ function Square(props) {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
           history: history.concat([{
-            squares: squares
+            squares: squares,
+            location: locations[i]
           }]),
           stepNumber: history.length,
           xIsNext: !this.state.xIsNext,
@@ -85,7 +98,7 @@ function Square(props) {
   
       const moves = history.map((step, move) => {
         const desc = move ?
-          'Go to move #' + move :
+          'Go to move #' + move + " at location (" + history[move].location + ")":
           'Go to game start';
         return (
             <li key={move}>
